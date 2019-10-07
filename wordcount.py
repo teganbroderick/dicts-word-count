eb2c375e76052cb13540f7722b478d23b7a9e956
Your Name <you@example.com>
2019-10-07 13:10:25 -0700

    Split main function into two functions

--- wordcount.py
+++ wordcount.py
@@ -7,19 +7,25 @@ def word_count(file_name):# put your code here.
         word_list = word_list + line
 
     # #Does not work, come back and fix this section
-    # for word in word_list:
-    #     word = word.replace(",", "")
-    #     word = word.replace(".", "")
-    #     word = word.replace("?", "")
+   # for word in word_list:
+   #      word.replace(",", "")
+   #  #     word = word.replace(".", "")
+   #  #     word = word.replace("?", "")
 
     word_counts = {}
     for word in word_list:
         word_counts[word] = word_counts.get(word, 0) + 1
 
+    return word_counts
 
-    for item, count in word_counts.items():
+
+def print_list(a_dictionary):
+    #make separate function for printing, take in dictionary as an argument
+    for item, count in a_dictionary.items():
         print(f'{item} {count}')
 
-    return ""
-#print(word_count("test.txt"))
-print(word_count("twain.txt"))
+    
+dictionary = word_count("test.txt")
+
+print_list(dictionary)
+#print(word_count("twain.txt"))
